631b3c1ebf688fd87a82281da2fbcb702b794600
Kajalkashyap1 <kajalkashyap539@gmail.com>
2025-02-01 11:03:11 +0530

    feat: added the caching mechanism

--- faq_project/faqs/models.py
+++ faq_project/faqs/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+from googletrans import Translator
 
 class FAQ(models.Model):
+    LANGUAGE_CHOICES = [
+        ('en', 'English'),
+        ('hi', 'Hindi'),
+        ('bn', 'Bengali'),
+    ]
+
     question = models.TextField()
     answer = RichTextField()  # WYSIWYG editor support
-    language = models.CharField(max_length=5, choices=[('en', 'English'), ('hi', 'Hindi'), ('bn', 'Bengali')])
+    language = models.CharField(max_length=5, choices=LANGUAGE_CHOICES, default='en')
+
+    def save(self, *args, **kwargs):
+        if self.language != 'en':  # Translate only non-English FAQs
+            translator = Translator()
+            self.question = translator.translate(self.question, src='en', dest=self.language).text
+            self.answer = translator.translate(self.answer, src='en', dest=self.language).text
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return self.question
+        return f"{self.question[:50]} ({self.language})"  # Show first 50 chars and language
